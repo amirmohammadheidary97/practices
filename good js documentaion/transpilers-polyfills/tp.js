cac6fa134583b7f6ab56866783f50008672158ca
amirmohammadheidary97 <amirmohammadheidary97@gmail.com>
2025-08-04 23:34:41 +0330

    ییی

---
+++ good js documentaion/transpilers-polyfills/tp.js
@@ -0,0 +1,10 @@
+const transpiller = `
+s2s . it reads and understands a source code and translates it to another source code . 
+ut can parse modern code and rewrite it in older syntax .
+`;
+
+const polyfill = `
+adds functions to an old js engine that doesnt have newer js engines functions . 
+A script that updates/adds new functions is called “polyfill”.
+It “fills in” the gap and adds missing implementations.
+`;
